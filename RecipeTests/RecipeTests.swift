388f41a6d23b4ce900628b049e0ec91d7d86b0e9
Sachit Mittal <sachitmittal073@gmail.com>
2025-10-06 13:22:19 +0530

    Initial Commit

---
+++ RecipeTests/RecipeTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeTests.swift
+//  RecipeTests
+//
+//  Created by Sachit Mittal on 06/10/25.
+//
+
+import Testing
+@testable import Recipe
+
+struct RecipeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
